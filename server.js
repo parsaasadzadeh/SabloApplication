68970a653e19221d977ae62ae292b1f7aac1348b
parsaasadzadeh <fightingparsa@gmail.com>
2026-06-22 17:33:37 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app');
-const connectDB = require('./src/config/db');
+
 const startCronJobs = require('./src/utils/cronJobs');
 
 const PORT = process.env.PORT || 5000;
 
-connectDB().then(() => {
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
         
         // روشن کردن کارگر همیشه بیدار سرور برای اقساط
         startCronJobs(); 
     });
-});
