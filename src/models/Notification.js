ac065a2aa9bdd1b054d343046fd5a44364c1b509
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-10 17:38:04 +0330

    Update Notification.js

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -1,15 +1,35 @@
 const mongoose = require('mongoose');
 
 const notificationSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    title: { type: String, required: true },
-    message: { type: String, required: true },
-    isRead: { type: Boolean, default: false }, // آیا کاربر پیام رو خونده؟
-    
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    isRead: {
+        type: Boolean,
+        default: false
+    }, // آیا کاربر پیام رو خونده؟
+
     // برای اینکه چک کنیم آیا قبلا برای این قسط پیام دادیم یا نه، آیدی قسط رو هم ذخیره می‌کنیم
-    relatedTransactionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Transaction', default: null },
-    
-    createdAt: { type: Date, default: Date.now }
+    relatedTransactionId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Transaction',
+        default: null
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 module.exports = mongoose.model('Notification', notificationSchema);
