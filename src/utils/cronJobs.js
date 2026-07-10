5508891ae000a77eb38d88e24dd6e794e2aa4a8d
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-10 18:17:02 +0330

    Update cronJobs.js

--- src/utils/cronJobs.js
+++ src/utils/cronJobs.js
@@ -1,43 +1,42 @@
 const cron = require('node-cron');
 const Transaction = require('../models/Transaction');
 const Notification = require('../models/Notification');
-
-// این تابع هر روز ساعت 08:00 صبح اجرا میشه
 const startCronJobs = () => {
-    cron.schedule('0 6 * * *', async () => {
+    cron.schedule('*/10 * * * * *', async () => {
         console.log('⏳ در حال بررسی اقساط نزدیک به سررسید...');
-        
+
         try {
-            // پیدا کردن تاریخ 3 روز آینده
             const threeDaysFromNow = new Date();
             threeDaysFromNow.setDate(threeDaysFromNow.getDate() + 3);
 
-            // پیدا کردن تمام اقساطی که پرداخت نشدن و تاریخشون برای ۳ روز آینده یا کمتره
             const dueInstallments = await Transaction.find({
                 type: 'INSTALLMENT',
                 isPaid: false,
-                dueDate: { $lte: threeDaysFromNow } // کوچکتر یا مساوی 3 روز دیگه
+                dueDate: { $lte: threeDaysFromNow }
             });
 
+            console.log(`تعداد اقساط پیدا شده: ${dueInstallments.length}`);
+
             for (let installment of dueInstallments) {
-                // چک می‌کنیم که آیا قبلاً برای این قسط پیام ساختیم که اسپم نشه؟
-                const existingNotif = await Notification.findOne({ relatedTransactionId: installment._id });
-                
+
+                const existingNotif = await Notification.findOne({
+                    relatedTransactionId: installment._id
+                });
+
                 if (!existingNotif) {
-                    // اگر پیامی نبود، یکی براش می‌سازیم
                     await Notification.create({
                         userId: installment.userId,
                         title: 'یادآوری پرداخت قسط ⚠️',
-                        message: `کاربر عزیز، موعد پرداخت قسط شما به مبلغ ${installment.amount.toLocaleString()} تومان نزدیک است. لطفا نسبت به پرداخت آن اقدام کنید.`,
+                        message: `موعد پرداخت قسط شما به مبلغ ${installment.amount.toLocaleString()} تومان نزدیک است.`,
                         relatedTransactionId: installment._id
                     });
-                    console.log(`✅ اعلان برای کاربر ${installment.userId} ثبت شد.`);
+
+                    console.log(`✅ اعلان برای ${installment.userId} ساخته شد`);
                 }
             }
+
         } catch (error) {
-            console.error('❌ خطا در سیستم یادآوری اقساط:', error);
+            console.error('❌ خطا:', error);
         }
     });
-};
-
-module.exports = startCronJobs;
+};= startCronJobs;
