c7627642a3c450848da5e55183b013d68b7967ff
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 13:25:47 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
     name: { type: String, default: "" }, 
     createdAt: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('User', userSchema);
