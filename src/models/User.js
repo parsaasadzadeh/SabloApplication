a10d81150e4ab822f225c66fc4394637dc8e4a9b
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 13:09:58 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
-    name: { type: String, default: "" },
-    createdAt: { type: Date, default: Date.now },
-
-    // توکن‌های پوش نوتیفیکیشن گوشی‌های این کاربر (ممکنه چند دستگاه داشته باشه)
-    pushTokens: { type: [String], default: [] },
+    name: { type: String, default: "" }, 
+    createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
