de6b016d001700e21a7dc22c9fbd06c77a3f0570
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 21:35:00 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
     name: { type: String, default: "" },
+    lastAiAnalysisAt: { type: Date, default: null },      // جدید
+    lastAiAnalysisResult: { type: String, default: '' },  // جدید
     createdAt: { type: Date, default: Date.now }
 });
 
