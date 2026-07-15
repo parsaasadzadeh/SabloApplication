92b0bd60e7abf6b7339c7a34b01ebb90d05c2696
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 17:09:33 +0330

    Update notificationRoutes.js

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getMyNotifications, markAsRead } = require('../controllers/notificationController');
+const {
+    getMyNotifications,
+    markAsRead,
+    registerPushToken,
+} = require('../controllers/notificationController');
 const { protect } = require('../middlewares/authMiddleware');
 
-router.use(protect); // همه روت‌های پیام نیاز به لاگین دارن
+router.use(protect);
 
 router.get('/', getMyNotifications);
 router.put('/:id/read', markAsRead);
+router.post('/register-push-token', registerPushToken);
 
 module.exports = router;
