a791c447efceb2110161d0e2882ac5750959a151
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 13:26:43 +0330

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,6 @@ const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/request-otp', requestOtp);
 router.post('/verify-otp', verifyOtp);
-
 // این روت نیاز به توکن داره، پس protect رو می‌ذاریم
 router.put('/complete-profile', protect, updateProfile);
 
