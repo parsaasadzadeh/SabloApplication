e795cc25ccb455e449e828365b5d000956196550
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-15 18:41:23 +0330

    Update cronJobs.js

--- src/utils/cronJobs.js
+++ src/utils/cronJobs.js
@@ -1,8 +1,5 @@
-// مسیر: src/utils/cronJobs.js
-
 const cron = require('node-cron');
 const { checkInstallments } = require('./checkInstallments');
-
 const startCronJobs = () => {
     cron.schedule('0 6 * * *', async () => {
         console.log('⏳ در حال بررسی اقساط نزدیک به سررسید...');
@@ -10,5 +7,4 @@ const startCronJobs = () => {
         console.log(`بررسی تمام شد: ${result.checked} قسط بررسی شد، ${result.created} اعلان ساخته شد.`);
     });
 };
-
 module.exports = startCronJobs;
