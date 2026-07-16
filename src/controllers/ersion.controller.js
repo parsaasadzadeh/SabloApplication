b5b48ef987071a32a27109a8883ef34d2efefc0e
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-17 01:09:16 +0330

    Create ersion.controller.js

---
+++ src/controllers/ersion.controller.js
@@ -0,0 +1,12 @@
+exports.checkVersion = (req, res) => {
+  try {
+    res.status(200).json({
+      latestVersion: process.env.APP_LATEST_VERSION || '1.0.0',
+      minVersion: process.env.APP_MIN_VERSION || '1.0.0',
+      storeUrl: process.env.APP_STORE_URL || 'myket://details?id=com.parsaas.Sablo',
+      message: 'نسخه جدید برنامه حسابداری Sablo منتشر شد. برای تجربه بهتر و امنیت بیشتر، برنامه را بروزرسانی کنید.'
+    });
+  } catch (error) {
+    res.status(500).json({ message: "خطا در بررسی نسخه اپلیکیشن" });
+  }
+};
