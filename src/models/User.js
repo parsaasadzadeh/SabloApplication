76a1d080bad78ae9ccd05d011ad9f63f582bf52f
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-16 12:42:28 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
+const pushTokenSchema = new mongoose.Schema({
+    token: { type: String, required: true },
+    platform: { type: String, enum: ['ios', 'android'], required: true },
+    updatedAt: { type: Date, default: Date.now },
+}, { _id: false });
+
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
     name: { type: String, default: "" },
-    lastAiAnalysisAt: { type: Date, default: null },      // جدید
-    lastAiAnalysisResult: { type: String, default: '' },  // جدید
+    lastAiAnalysisAt: { type: Date, default: null },      
+    lastAiAnalysisResult: { type: String, default: '' }, 
+     pushTokens: { type: [pushTokenSchema], default: [] }, // ج
     createdAt: { type: Date, default: Date.now }
 });
 
