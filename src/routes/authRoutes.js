207d9fdb27d7b6bae573a0b5026990b1ceacb23b
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-21 18:17:08 +0330

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { requestOtp, verifyOtp, updateProfile } = require('../controllers/authController');
+const { requestOtp, verifyOtp, updateProfile, savePushToken } = require('../controllers/authController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.post('/request-otp', requestOtp);
@@ -8,6 +8,7 @@ router.post('/verify-otp', verifyOtp);
 // این روت نیاز به توکن داره، پس protect رو می‌ذاریم
 router.put('/complete-profile', protect, updateProfile);
 
-router.put('/push-token', protect, savePushToken); // 👈 اضافه کن
+// src/routes/authRoutes.js — یه خط اضافه کن
 
+router.put('/push-token', protect, savePushToken); // 👈 اضافه کن
 module.exports = router;
