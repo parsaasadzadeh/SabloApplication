324781936348cd86bd2afd0c8a2ee4813a15cd36
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-26 18:36:04 +0330

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
         
-        // روشن کردن کارگر همیشه بیدار سرور برای اقساط
         startCronJobs(); 
     });
