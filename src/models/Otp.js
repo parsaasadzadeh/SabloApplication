21f1c2f496747663275dff050941b916e485146f
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-26 18:41:58 +0330

    Update Otp.js

--- src/models/Otp.js
+++ src/models/Otp.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const otpSchema = new mongoose.Schema({
     phone: { type: String, required: true },
     code: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now, expires: 120 } // انقضا بعد از 120 ثانیه
+    createdAt: { type: Date, default: Date.now, expires: 120 } 
 });
 
 module.exports = mongoose.model('Otp', otpSchema);
