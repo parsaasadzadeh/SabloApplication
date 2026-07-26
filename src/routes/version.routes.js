148f188d953aec89338aac575f42abed18b3ee3f
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-26 18:38:20 +0330

    Update version.routes.js

--- src/routes/version.routes.js
+++ src/routes/version.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const versionController = require('../controllers/version.controller');
 
-// مسیر: GET /api/version/check
 router.get('/check', versionController.checkVersion);
 
 module.exports = router;
