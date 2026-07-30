b48c166c865f66e39359752bb5ed69a7a5a38368
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-30 16:21:28 +0330

    Update notificationController.js

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -1,16 +1,17 @@
 const Notification = require('../models/Notification');
+// دریافت لیست پیام‌های کاربر
 exports.getMyNotifications = async (req, res) => {
     try {
         const notifications = await Notification.find({ userId: req.user.id })
-            .sort({ createdAt: -1 }) 
-            .limit(20); 
+            .sort({ createdAt: -1 }) // جدیدترین‌ها اول باشن
+            .limit(20); // فقط 20 تای آخر رو بده که اپ سنگین نشه
         const unreadCount = await Notification.countDocuments({ userId: req.user.id, isRead: false });
         res.status(200).json({ unreadCount, notifications });
     } catch (error) {
         res.status(500).json({ message: 'خطای سرور', error: error.message });
     }
 };
-
+// تغییر وضعیت پیام به خوانده شده (وقتی کاربر پیام رو باز میکنه)
 exports.markAsRead = async (req, res) => {
     try {
         await Notification.findByIdAndUpdate(req.params.id, { isRead: true });
@@ -19,3 +20,4 @@ exports.markAsRead = async (req, res) => {
         res.status(500).json({ message: 'خطای سرور', error: error.message });
     }
 };
+
