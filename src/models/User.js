7fe8ec3c7d429e96dc2b8ea4c1aa61f753cda9d6
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-30 16:01:16 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
     name: { type: String, default: "" },
-    expoPushToken: { type: String, default: null }, 
     lastAiAnalysisAt: { type: Date, default: null },      
     lastAiAnalysisResult: { type: String, default: '' }, 
     createdAt: { type: Date, default: Date.now }
