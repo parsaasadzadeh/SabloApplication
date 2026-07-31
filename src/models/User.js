eba9efeaa4894b8d129a236f5bee34c228cd7b09
parsaasadzadeh <fightingparsa@gmail.com>
2026-07-31 23:31:04 +0330

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
     name: { type: String, default: "" },
+    currency: { 
+        type: String, 
+        enum: ['IRR', 'IRT'],
+        default: 'IRT'
+    },
     lastAiAnalysisAt: { type: Date, default: null },      
     lastAiAnalysisResult: { type: String, default: '' }, 
     createdAt: { type: Date, default: Date.now }
