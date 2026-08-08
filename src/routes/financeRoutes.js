54143c30d8ab7840badb5865cf7d7d6bcc8380a0
parsaasadzadeh <fightingparsa@gmail.com>
2026-08-09 01:56:56 +0330

    Update financeRoutes.js

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { addTransaction, getMyTransactions, getFinanceStats, payInstallment,deleteTransaction, updateTransaction } = require('../controllers/financeController');
+const { 
+    addTransaction, 
+    getMyTransactions, 
+    getFinanceStats, 
+    payInstallment,
+    deleteTransaction, 
+    updateTransaction,
+    getMonthlyComparison  // ✅ اضافه شد
+} = require('../controllers/financeController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.use(protect); 
-
 router.post('/add', addTransaction);
 router.get('/my-data', getMyTransactions);
 router.get('/stats', getFinanceStats); 
-router.put('/pay-installment/:id', payInstallment); //   پرداخت قسط
+router.put('/pay-installment/:id', payInstallment);
 router.put('/update/:id', updateTransaction);  
-router.delete('/delete/:id', deleteTransaction);  
-
+router.delete('/delete/:id', deleteTransaction);
+router.get('/monthly-comparison', getMonthlyComparison); // ✅ اضافه شد
   
 module.exports = router;
