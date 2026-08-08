8e785b25a77c9050a8ef148996baf9def3134bd4
parsaasadzadeh <fightingparsa@gmail.com>
2026-08-08 17:54:55 +0330

    Update version.controller.js

--- src/controllers/version.controller.js
+++ src/controllers/version.controller.js
@@ -1,8 +1,8 @@
 exports.checkVersion = (req, res) => {
   try {
     res.status(200).json({
-      latestVersion: process.env.APP_LATEST_VERSION || '2.0.3',
-      minVersion: process.env.APP_MIN_VERSION || '2.0.1',
+      latestVersion: process.env.APP_LATEST_VERSION,
+      minVersion: process.env.APP_MIN_VERSION,
       forceUpdate: process.env.FORCE_UPDATE_ENABLED === 'true',
       storeUrl: process.env.APP_STORE_URL || 'https://myket.ir/app/com.sabloapp.sablo',
       message: 'نسخه جدید برنامه حسابداری Sablo منتشر شد. برای تجربه بهتر و امنیت بیشتر، برنامه را بروزرسانی کنید.'
