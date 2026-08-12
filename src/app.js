78bdb6f19a4351af6c7fb55060de744c196f1d17
parsaasadzadeh <fightingparsa@gmail.com>
2026-08-13 01:36:39 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const aiRoutes = require('./routes/aiRoutes');
 const notificationRoutes = require('./routes/notificationRoutes'); 
 const versionRoutes = require('./routes/version.routes');
 const cronRoutes = require('./routes/cronRoutes'); 
+const goalRoutes = require('./routes/goalRoutes');
 const connectDB = require('./config/db');
 
 
@@ -22,4 +23,5 @@ app.use('/api/notifications', notificationRoutes);
 app.use('/api/cron', cronRoutes);  
 app.use('/api/ai', aiRoutes);
 app.use('/api/version', versionRoutes);
+app.use('/api/goals', goalRoutes);
 module.exports = app;
