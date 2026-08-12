35ca98ede0317787342fc5964cb04ec281ddc0be
parsaasadzadeh <fightingparsa@gmail.com>
2026-08-13 01:42:04 +0330

    Update financeRoutes.js

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -8,7 +8,7 @@ router.use(protect);
 router.post('/add', addTransaction);
 router.get('/my-data', getMyTransactions);
 router.get('/stats', getFinanceStats); 
-router.put('/pay-installment/:id', payInstallment); //   پرداخت قسط
+router.put('/pay-installment/:id', payInstallment); 
 router.put('/update/:id', updateTransaction);  
 router.delete('/delete/:id', deleteTransaction);  
 router.get('/monthly-comparison', getMonthlyComparison);
