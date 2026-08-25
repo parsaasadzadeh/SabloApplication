ce0f39e6c68c1408fb572a1b86f5d07313eeb6fb
parsaasadzadeh <fightingparsa@gmail.com>
2026-08-25 20:12:38 +0330

    Update financeRoutes.js

--- src/routes/financeRoutes.js
+++ src/routes/financeRoutes.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { addTransaction, getMyTransactions,exportTransactionsCSV, getFinanceStats, payInstallment,deleteTransaction, updateTransaction, getMonthlyComparison } = require('../controllers/financeController');
+const {
+    addTransaction,
+    getMyTransactions,
+    exportTransactionsCSV,
+    getFinanceStats,
+    payInstallment,
+    deleteTransaction,
+    updateTransaction,
+    getMonthlyComparison,
+    getCategoryStats,
+    getCategories,
+} = require('../controllers/financeController');
 const { protect } = require('../middlewares/authMiddleware');
 
-router.use(protect); 
+router.use(protect);
 
 router.post('/add', addTransaction);
 router.get('/my-data', getMyTransactions);
-router.get('/stats', getFinanceStats); 
-router.put('/pay-installment/:id', payInstallment); 
-router.put('/update/:id', updateTransaction);  
-router.delete('/delete/:id', deleteTransaction);  
+router.get('/stats', getFinanceStats);
+router.put('/pay-installment/:id', payInstallment);
+router.put('/update/:id', updateTransaction);
+router.delete('/delete/:id', deleteTransaction);
 router.get('/monthly-comparison', getMonthlyComparison);
 router.get('/export-csv', exportTransactionsCSV);
-  
-module.exports = router;
+router.get('/categories', getCategories);
+router.get('/category-stats', getCategoryStats);
 
+module.exports = router;
