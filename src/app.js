0a4e48c2c98ad5240c3bde3db620d89b5bb49fbf
parsaasadzadeh <fightingparsa@gmail.com>
2026-09-03 15:00:19 +0330

    Update app.js

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cache-Control, Pragma, Expires');
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
   }
