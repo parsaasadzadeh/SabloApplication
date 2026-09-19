22b512a76ccb5740d90c2920e23d33d0ca4d93ca
parsaasadzadeh <fightingparsa@gmail.com>
2026-09-19 12:06:16 +0330

    Update cardRoutes.js

--- src/routes/cardRoutes.js
+++ src/routes/cardRoutes.js
@@ -1,7 +1,7 @@
 // routes/cardRoutes.js
 const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middlewares/authMiddleware');
 const {
     getCards,
     createCard,
